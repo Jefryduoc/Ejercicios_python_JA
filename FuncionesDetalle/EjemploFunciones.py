306f04a6fe605702fb14817ee6ff3b88a56af53f
Jefryduoc <jeo.aravena@duocuc.cl>
2026-05-30 11:34:58 -0400

    funciones en otro archivo

---
+++ FuncionesDetalle/EjemploFunciones.py
@@ -0,0 +1,15 @@
+import FuncionesDetalle as fn
+#from funcionDetalle import sumar
+
+
+#linea ppal
+fn.sumarDosNumeros()
+
+res= fn.sumar(4,5)
+print(res)
+
+num1= int(input("numero 1 :  "))
+num2= int(input("numero 2 :  "))
+res= fn.sumar(num1,num2)
+print("la suma es :", res)
+
--- FuncionesDetalle/funciones.py
+++ FuncionesDetalle/funciones.py
@@ -16,15 +16,3 @@ def sumar(a,b):
 
     suma= a + b 
     return suma
-
-#linea ppal
-sumarDosNumeros()
-
-res= sumar(4,5)
-print(res)
-
-num1= int(input("numero 1 :  "))
-num2= int(input("numero 2 :  "))
-res= sumar(num1,num2)
-print("la suma es :", res)
-
